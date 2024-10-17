cbaef20e3823e1bb6fc4464a85d8dc91f7f2ca06
luboshellesch <lubos.hellesch@gmail.com>
2024-10-17 16:28:19 +0200

    Touch detection on screen.

--- Assets/Scripts/TouchDetection.cs
+++ Assets/Scripts/TouchDetection.cs
@@ -4,14 +4,15 @@ public class TouchDetection : MonoBehaviour
 {
     [SerializeField] private GameObject markerPrefab;
     [SerializeField] private GameObject screen;
+    private const float heightOfMarker = 0.00455f;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Hello!");
         if (other.CompareTag("IndexFinger"))
         {
             Vector3 touchPosition = other.ClosestPoint(screen.transform.position);
-            Instantiate(markerPrefab, touchPosition, Quaternion.identity);
+            Vector3 markerPosition = new Vector3(touchPosition.x, screen.transform.position.y + heightOfMarker,touchPosition.z);
+            Instantiate(markerPrefab, markerPosition, Quaternion.identity);
         }
     }
 }
